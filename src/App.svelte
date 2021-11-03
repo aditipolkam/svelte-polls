0a7f84700d1c5876910d4769fd2aca3e8c59c277
aditipolkam <aditipolkam@gmail.com>
2021-11-03 23:03:27 +0530

    ADDED Header component

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,16 @@
 <script>
+	import Header from './components/Header.svelte';
 </script>
 
 <main>
+	<Header/>
 	<h1>Hello world!</h1>
-	
+	<p>elfiyr489fgavtly54 skeyrfv v uehrfbyey vhrgfkvewbgrvuwhe yvryechgwew ueyrvbuhrf vuwej uyrvuv hbeurbgtreu yrgty ughuergtfuerf gfyurfhyurhykyug	8yu58yt58bhy54luiyh4u5byh by8ty589by5383yu58t 835yt835vnyt785yvt835 45yt598vbyt58yt853i t8u5ty589v85yt853b 38ut8n35y85y835y</p>
 </main>
 
 <style>
-	
+	main{
+		max-width: 960px;
+		margin: 40px auto;
+	}
 </style>

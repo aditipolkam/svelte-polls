60e8acdfa874b47118e8c4da90f42d554b1cdcf5
aditipolkam <aditipolkam@gmail.com>
2021-11-06 12:38:58 +0530

    ADDED a form to create poll

--- src/App.svelte
+++ src/App.svelte
@@ -2,6 +2,8 @@
 	import Header from './components/Header.svelte';
 	import Footer from './components/Footer.svelte';
 	import Tabs from './shared/Tabs.svelte';
+	import CreatePollForm from './components/CreatePollForm.svelte';
+	
 	//tabs
 	let items = ['Current Polls','Add New Poll'];
 	let activeItem = 'Current Polls';
@@ -17,7 +19,7 @@
 	{#if activeItem === 'Current Polls'}
 		<p>Polls Lists Component</p>
 	{:else if activeItem === 'Add New Poll'}
-		<p>Add New Poll Component</p>
+		<CreatePollForm/>
 	{/if}
 </main>
 <Footer />
